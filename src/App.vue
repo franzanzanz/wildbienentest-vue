d7880aa040512594605c4a47b5fe795a01bfcc3c
Franz Rosenberger <mail@franzrosenberger.de>
2019-08-29 11:55:54 +0200

    Font import

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,11 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+@import "./assets/sass/_font-imports";
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;

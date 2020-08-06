cab654e449bd72752bfa28acff225372f285cc11
Franz Rosenberger <mail@franzrosenberger.de>
2020-08-06 18:31:50 +0200

    footer component credit list added

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,10 @@
 	<div class="container-fluid py-5 footer">
 		<div class="row">
 			<div class="col-12 col-md-4 text-center text-md-left mb-4 mb-md-0">
-				<router-link class="textlink" to="/imprint">
+				<router-link
+					class="textlink"
+					to="/imprint"
+				>
 					Datenschutz / Impressum
 				</router-link>
 				<div class="dash pb-3 mb-3 mx-auto mx-md-0" />
@@ -11,12 +14,21 @@
 				</p>
 			</div>
 			<div class="col-12 col-md-4 text-center">
-				<img class="logo-footer" src="../assets/media/logo-wildbienentest.svg" alt="">
+				<router-link
+					to="/"
+				>
+					<img class="logo-footer" src="../assets/media/logo-wildbienentest.svg" alt="">
+				</router-link>
 			</div>
 		</div>
-		<ol>
-			<li v-for="item in imageSources" :key="item.number" v-html="item.content" />
-		</ol>
+		<div class="row">
+			<ol class="col credits-list mt-5">
+				<p class="credits-list__headline mb-2">
+					<strong>Bildnachweise, absteigend nach Einsatz auf dieser Seite</strong>
+				</p>
+				<li v-for="item in imageSources" :key="item.number" v-html="item.content" />
+			</ol>
+		</div>
 	</div>
 </template>
 
@@ -47,29 +59,41 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
+<style lang="scss">
 	@import "../assets/sass/_colors.scss";
+	@import "../assets/sass/_variables.scss";
 
 	.footer {
-		background-color: $bt-yellow-ultralight;
+		background-color: $yellow-ultralight;
 		border: solid transparent;
 		border-width: 4px 0 0 0; /* border-bottom only */
 		border-image-source: url('../../src/assets/media/border-blobs.svg');
 		border-image-slice: 20%; /* no unit for px, you can use % */
 		border-image-repeat: repeat; /* the middle slices will be repeated cleanly up to the edges */
+
+
+		.dash {
+			border-bottom: 1px solid $border-grey;
+			width: 4rem;
+		}
+
+		.logo-footer {
+			width: 5.5rem;
+		}
+
+		.credits-list {
+			list-style-position: inside;
+			a {
+				color: $red;
+				&:hover {
+					color: $red-darker;
+					text-decoration: none;
+				}
+			}
+		}
+
+		.credits-list, .credits-list__headline {
+			font-size: $font-size-credits;
+		}
 	}
-
-	.dash {
-		border-bottom: 1px solid $bt-border-grey;
-		width: 4rem;
-	}
-
-	.logo-footer {
-		width: 5.5rem;
-	}
-
-	.textlink-footer{
-
-	}
-
 </style>
